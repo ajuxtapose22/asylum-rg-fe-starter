3adf63fb20c9d65af956b055350c6ce495860b02
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-11-30 08:41:14 -0700

    Bottom Section content with styling complete and links functional. Implementing graph logic to be able to function properly.

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -21,6 +21,7 @@ function RenderLandingPage(props) {
   return (
     <div className="main">
       <div className="header">
+        
         <div className="header-text-container">
           <h1>Asylum Office Grant Rate Tracker</h1>
           <h3>
@@ -92,7 +93,43 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+
         <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className='data-container'>
+          <div className='first-data-point-container'>
+            <h2>36%</h2>
+            <h3>
+            By the end of the Trump administration, the average asylum office grant rate had fallen 36 percent from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+            </h3>
+          </div>
+          <div className='second-data-point-container'>
+            <h2>5%</h2>
+            <h3>
+            The New York asylum office grant rate dropped to 5 percent in fiscal year 2020.</h3>
+          </div>
+          <div className='first-data-point-container'>
+            <h2>6x Lower</h2>
+            <h3>
+            Between fiscal year 2017 and 2020, the New York asylum office’s average grant rate was six times lower than the San Francisco asylum office.</h3>
+          </div>
+
+          </div>
+        {/* READ MORE BUTTON */}
+        <div className="read-more-data-btn-container">
+      
+              <Button
+                type="default"
+                style={{ backgroundColor: '#404C4A', color: '#FFFFFF', marginRight: '10px' }}
+                onClick={() => window.location.href = 'https://humanrightsfirst.org/library/uscis-records-reveal-systemic-disparities-in-asylum-decisions/'
+                }
+              >
+                Read More
+              </Button>
+
+        </div>
+
+        {/* BACK TO TOP BUTTON */}
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
